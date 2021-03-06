a312c9ec9433edf69b9b667910413df475f4dd9b
Ankit Kumar <ankit.tec40@gmail.com>
2021-03-06 17:11:56 +0530

    first

---
+++ Python/HackerRank/MinionGame.py
@@ -0,0 +1,29 @@
+def minion_game(s):
+    player1="Kevin"
+    player2="Stuart"
+    player1Score=0
+    player2Score=0
+   # while(True):
+    for i in range(2):
+         if(i==0):  #player1chance
+                word1=input()
+
+        else:
+            word2=input()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
