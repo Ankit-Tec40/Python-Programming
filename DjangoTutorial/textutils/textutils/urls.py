401bc5269fca25aa895eb44b43b5e20ebf21c54e
Ankit Kumar <ankit.tec40@gmail.com>
2021-03-09 20:10:41 +0530

    932021

--- DjangoTutorial/textutils/textutils/urls.py
+++ DjangoTutorial/textutils/textutils/urls.py
@@ -16,10 +16,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
     path('about',views.about,name='about')
 
 ]
+"""
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('removepunc',views.removepunc,name='Removepunc'),
+    path('capatalizefirst',views.capfirst,name='capfirst'),
+    path('newlineremove',views.newlineremove,name='newlineremove'),
+    path('spaceremove',views.spaceremove,name='spaceremove'),
+    path('charcount',views.charcount,name='charcount'),
+
+
+
+
+
+]
+
--- DjangoTutorial/textutils/textutils/views.py
+++ DjangoTutorial/textutils/textutils/views.py
@@ -1,5 +1,7 @@
 #this created by Ankit
 from django.http import HttpResponse
+from django.shortcuts import render
+"""
 def index(request):
     f=open('text.txt','r')
     text=f.read()
@@ -8,3 +10,19 @@ def index(request):
 
 def about(request):
     return HttpResponse("about Ankit")
+"""
+def index(request):
+    #return HttpResponse("Home")
+    return render(request,'index.html')
+
+
+def removepunc(request):
+    return HttpResponse("Remove punc")
+def capfirst(request):
+    return HttpResponse("capitalize first")
+def newlineremove(request):
+    return HttpResponse("newlineremover")
+def spaceremove(request):
+    return HttpResponse("space remover")
+def charcount(request):
+    return HttpResponse("char count")
