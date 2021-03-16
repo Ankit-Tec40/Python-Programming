0887f0707782f96483a58978c168b3924869e89e
Ankit Kumar <ankit.tec40@gmail.com>
2021-03-17 01:16:36 +0530

    1732021

---
+++ SelfProjects/PostOffice/Agent.py
@@ -0,0 +1,2 @@
+# Class Agentdata:
+
--- SelfProjects/PostOffice/Users.py
+++ SelfProjects/PostOffice/Users.py
@@ -1,23 +1,26 @@
+# Import time module
 import datetime
+# import relativedlta module for calculation between time and dates
 from dateutil.relativedelta import relativedelta
-
-def dates():
+# This function take input for time and handle its exceptions
+def inputdate():
     day,month,year=map(int,input("Opening Date (dd/mm/yyyy) : ").split("/"))
     openingdate=datetime.datetime(year,month,day)
+    return(openingdate)
+# This function Calcute the date after defined months
+def calcdate(openingdate):
     openingdatedisplay=openingdate.strftime("%d/%m/%Y")
     closingdate=openingdate+ relativedelta(months=+60)
     closingdatedisplay=closingdate.strftime("%d/%m/%Y")
     print(openingdatedisplay)
     print(closingdatedisplay)
-
-def amountcalc(amount):
+# This Function Calculate the total amount with the interest
+def calcamount(amount):
     interest=5.8
     finalamount=amount*interest
     print(finalamount)
 
-
-
-
+# This is the main user class
 class users:
     def get(self):
         accNo=input("Account No : ")
@@ -25,15 +28,13 @@ class users:
         nomeniName=input("Nomeni Name : ")
         address=input("Address : ")
         amount=int(input("Amount : "))
-        dates()
-        amountcalc(amount)
+        openingdate=inputdate()
+        calcdate(openingdate)
+        calcamount(amount)
 
 
 
-def main():
+# From here code starts excuting
+if __name__=="__main__":
     ob=users()
     ob.get()
-
-
-if __name__=="__main__":
-    main()
